2aa68112335f5b8bbacb8d5c6fa5b0ecde0933d1
Adil Sadout <adelsadout@hotmail.com>
2019-10-29 16:24:35 +0100

    added javascript to the hamburger button to open and close the mobile nav

--- css course/javascript/script.js
+++ css course/javascript/script.js
@@ -1,22 +1,52 @@
 var backdrop = document.querySelector('.main-backdrop');
 var modal = document.querySelector('.main-modal');
 var selectPlanButtons = document.querySelectorAll('.main-section__product .product-button');
-var modalNoButton = document.querySelector('.main-modal__action--negative')
-console.dir(selectPlanButtons);
-console.dir(modal);
+var modalNoButton = document.querySelector('.main-modal__action--negative');
+var hamburgerButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+var buttonXclose = document.querySelector('.close-xbutton');
+
+
+buttonXclose.addEventListener('click', function(){
+	mobileNav.style.display = 'none';
+	backdrop.style.display = 'none';
+});
+
+hamburgerButton.addEventListener('click', function(){
+	mobileNav.style.display = 'block';
+	backdrop.style.display = 'block';
+});
+
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
 	selectPlanButtons[i].addEventListener('click', function() {
 		modal.style.display = 'block';
 		backdrop.style.display = 'block';
 	})
-}
+};
+
+backdrop.addEventListener('click', function(){
+	
+	backdrop.style.display = 'none';
+
+	if (modal.style.display != 'none') {
+		modal.style.display = 'none';
+	}
+
+	if (mobileNav.style.display != 'none'){
+		mobileNav.style.display = 'none';
+	};
+	
+});
+
 
 modalNoButton.addEventListener('click', function(){
 	modal.style.display = 'none';
 	backdrop.style.display = 'none';
-})
+});
 
-//Trying a to make the modal's 'no' button close the modal itself and remove the backdrop
+
+
+
 
 
